127bb8b45c75885ee69abb8daa56c6618b9592ce
ESPINAR LUCAS <lespinar@sfasp2mi.univ-poitiers.fr>
2023-11-21 17:07:58 +0100

    Change name of Class (without the S)

--- com/utils/Command.java
+++ com/utils/Command.java
@@ -27,7 +27,9 @@ package com.utils;
  *      —USE arg1 [arg2]: uses the object arg1. In case a second argument is given, the first
  *                      one is used with the second. For example, use gun bullet may load the gun, which
  *                      can be used after that.
+ *      -GETLOCATION : 
  */
+
 public class Commands {
 
     static String[] cmd = {"GO", "HELP", "LOOK", "TAKE", "QUIT"};
