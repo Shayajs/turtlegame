bf8f14c781ee99b5649dbf9f6fe55d13c93afa2d
Lou <louisemouret86@gmail.com>
2023-11-22 15:13:06 +0100

    constructeur location

--- com/world/Location.java
+++ com/world/Location.java
@@ -8,9 +8,12 @@ import java.util.Map;
 public class Location {
     private String name;
     private Map<String, Exit> exits = new HashMap<>();
+    private Inventory inventory;
 
-    public Location() {
-        
+    public Location(n String, exits Map<String, Exit>, inventory Inventory) {
+        this.name = n;
+        this.exits = exits;
+        this.inventory = inventory;
     }
 
 }
